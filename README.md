36d9fa4ca4684f29eab7ded4b3c5dba5ba73f883
yangyi_peng <yangyi_peng@htc.com>
2017-07-26 16:53:49 +0800

    [github-toc] Add source code and the README.md which show the usage
    Symptom: Add source code and the README.md which show the usage
    Root Cause: Add source code and the README.md which show the usage
    Solution: Add source code and the README.md which show the usage
    Project: github-toc
    Note: N/A

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
+[github-toc](#github-toc)  
+　1. [level 2 1st](#level-2-1st)  
+　　1.1 [level 3 1st](#level-3-1st)  
+　　　　1.1.1 [level 4 1st](#level-4-1st)  
+　　　　　　1.1.1.1 [level 5 1st](#level-5-1st)  
+　　　　　　　　1.1.1.1.1 [level 6 1st](#level-6-1st)  
+　　　　　　　　1.1.1.1.2 [level 6 2nd](#level-6-2nd)  
+　　　　　　　　1.1.1.1.3 [level 6 3rd](#level-6-3rd)  
+　　　　　　1.1.1.2 [level 5 2nd](#level-5-2nd)  
+　　　　　　　　1.1.1.2.1 [level 6 1st](#level-6-1st-1)  
+　　　　　　　　1.1.1.2.2 [level 6 2nd](#level-6-2nd-1)  
+　　　　　　1.1.1.3 [level 5 3rd](#level-5-3rd)  
+　　　　1.1.2 [level 4 2nd](#level-4-2nd)  
+　　　　　　1.1.2.1 [level 5 1st](#level-5-1st-1)  
+　　　　　　　　1.1.2.1.1 [level 6 1st](#level-6-1st-2)  
+　　1.2 [level 3 2nd](#level-3-2nd)  
+　　1.3 [level 3 3rd](#level-3-3rd)  
+　　1.4 [level 3 4th](#level-3-4th)  
+　2. [level 2 2nd](#level-2-2nd)  
+　　2.1 [level 3 1st](#level-3-1st-1)  
+　　　　2.1.1 [level 4 1st](#level-4-1st-1)  
+　　　　　　2.1.1.1 [level 5 1st](#level-5-1st-2)  
+　　　　　　　　2.1.1.1.1 [level 6 1st](#level-6-1st-3)  
+　3. [Usage](#usage)  
+  
 # github-toc
 Help to generate the TOC of the README.md of your project
+  
+## level 2 1st  
+### level 3 1st  
+#### level 4 1st  
+##### level 5 1st  
+###### level 6 1st  
+###### level 6 2nd
+###### level 6 3rd  
+##### level 5 2nd
+###### level 6 1st  
+###### level 6 2nd  
+##### level 5 3rd  
+#### level 4 2nd  
+##### level 5 1st  
+###### level 6 1st  
+### level 3 2nd  
+### level 3 3rd  
+### level 3 4th  
+## level 2 2nd
+### level 3 1st  
+#### level 4 1st  
+##### level 5 1st  
+###### level 6 1st  
+## Usage
+Copy the README.md to the folder with genTOC.py.
+If README.md.bak already exist before generating the TOC, please delete the old README.md.bak.
+Then you can exec ```Python3 genTOC.py```
---
+++ genTOC.py
@@ -0,0 +1,151 @@
+import os, re, sys, shutil
+
+FIRST = re.compile('^# ')
+SECOND = re.compile('^## ')
+THIRD = re.compile('^### ')
+FOURTH = re.compile('^#### ')
+FIFTH = re.compile('^##### ')
+SIXTH = re.compile('^###### ')
+
+DELETE = re.compile('\(|\)|\/')
+SPACE = re.compile(' ')
+DELSPACE = re.compile('^ *| *$')
+NEWLINE = re.compile('\n')
+ICON = re.compile(' \[.*$')
+MD = re.compile('<.*?>|<\/.*?>')
+
+LEVEL1 = "["
+LEVEL2 = "　"
+LEVEL3 = "　　"
+LEVEL4 = "　　　　"
+LEVEL5 = "　　　　　　"
+LEVEL6 = "　　　　　　　　"
+
+content = []
+each = []
+origin = []
+org_each = []
+
+count_dict = {}
+
+def enqueue(Q, e):
+    Q.insert(len(Q), e)
+
+def dequeue(Q):
+    return Q.pop(0)
+
+def backup():
+    try:
+        if not os.path.exists("README.md.bak"):
+            shutil.copy("README.md", "README.md.bak")
+    except:
+        print("Cannot backup the file")
+
+def findTitle(NO, string, LEVEL):
+    global each, org_each
+    if re.search(NO, string):
+        if re.search(ICON, string):
+            string = re.sub(ICON, '', string)
+            string = string + '-'
+        title = re.sub(NO, '', string)
+        title = re.sub(DELSPACE, '', title)
+        title = re.sub(NEWLINE, '', title)
+        org_dict = {}
+        org_dict[LEVEL] = re.sub('-$', '', title)
+        enqueue(org_each, org_dict)
+        
+        title = re.sub(MD, '', title)
+        title = re.sub(SPACE, '-', title)
+        title = re.sub(DELETE, '', title)
+
+        if title not in count_dict.keys():
+            count_dict[title] = 1
+        else:
+            new = title + "-" + str(count_dict[title])
+            count_dict[title] += 1
+            title = new
+
+        title_dict = {}
+        title_dict[LEVEL] = title.lower()
+        enqueue(each, title_dict)
+
+def genTOC(content, org):
+    string = ""
+    count_list = [0, 0, 0, 0, 0,]
+    for i, v in enumerate(content):
+        if list(v.keys())[0] == 1:
+            string = LEVEL1 + list(org[i].values())[0] + '](#' + list(v.values())[0] + ')' + "  \n"
+        elif list(v.keys())[0] == 2:
+            count_list[0] += 1
+            count_list[1] = 0
+            string += LEVEL2 + str(count_list[0]) + ". ["  + list(org[i].values())[0] + '](#' + list(v.values())[0] + ')' + "  \n"
+        elif list(v.keys())[0] == 3:
+            count_list[1] += 1
+            count_list[2] = 0
+            string += LEVEL3 + str(count_list[0]) + "." + str(count_list[1]) + " [" + list(org[i].values())[0] + '](#' + \
+                      list(v.values())[0] + ')' + "  \n"
+        elif list(v.keys())[0] == 4:
+            count_list[2] += 1
+            count_list[3] = 0
+            string += LEVEL4 + str(count_list[0]) + "." + str(count_list[1]) + "." + str(count_list[2]) + " [" + \
+                      list(org[i].values())[0] + '](#' + list(v.values())[0] + ')' + "  \n"
+        elif list(v.keys())[0] == 5:
+            count_list[3] += 1
+            count_list[4] = 0
+            string += LEVEL5 + str(count_list[0]) + "." + str(count_list[1]) + "." + str(count_list[2]) + "." + \
+                      str(count_list[3]) + " [" + list(org[i].values())[0] + '](#' + list(v.values())[0] + ')' + "  \n"
+        elif list(v.keys())[0] == 6:
+            count_list[4] += 1
+            string += LEVEL6 + str(count_list[0]) + "." + str(count_list[1]) + "." + str(count_list[2]) + "." + \
+                      str(count_list[3]) + "." + str(count_list[4]) + " [" + list(org[i].values())[0] + '](#' + list(v.values())[0] + ')' + "  \n"
+
+    return string
+
+def getTitles():
+    global each, org_each
+    contentStr = ""
+    try:
+        with open("README.md", "r", encoding='UTF-8') as f:
+            for line in f.readlines():
+                if re.search(FIRST, line):
+                    if each:
+                        content.append(each)
+                        origin.append(org_each)
+                        each = []
+                        org_each = []
+                findTitle(FIRST, line, 1)
+                findTitle(SECOND, line, 2)
+                findTitle(THIRD, line, 3)
+                findTitle(FOURTH, line, 4)
+                findTitle(FIFTH, line, 5)
+                findTitle(SIXTH, line, 6)
+        if each:
+            content.append(each)
+        if org_each:
+            origin.append(org_each)
+
+        for i, v in enumerate(content):
+            contentStr += genTOC(v, origin[i])
+
+        return contentStr
+    except:
+        print(sys.exc_info())
+        sys.exit()
+
+def writeContent(string):
+    string += "  \n"
+    try:
+        with open("README.md", "w", encoding='UTF-8') as f:
+            f.write(string)
+            with open("README.md.bak", "r", encoding='UTF-8') as read:
+                for i in read.readlines():
+                    f.write(i)
+
+    except:
+        print(sys.exc_info())
+        sys.exit()
+
+backup()
+string = getTitles()
+writeContent(string)
+
